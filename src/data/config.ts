1775a7e71c4a64aaa90f30c8c7efae93bb9d4cda
Samuel Leite <samuelleiten@gmail.com>
2021-09-17 14:03:22 -0300

    project finish

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,7 +1,7 @@
 import firebase from "firebase";
 import "firebase/firestore";
 
-if (!firebase.apps.lenght) {
+if (!firebase.apps.length) {
   firebase.initializeApp({
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
